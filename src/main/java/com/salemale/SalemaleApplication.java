48e072ef3f2ac90e82d2f1856b23bf9578e1ebb4
beans3142 <greenbeans3142@naver.com>
2025-09-28 17:58:39 +0900

    refactor: BaseEntity 자동 업데이트 수정

--- src/main/java/com/salemale/SalemaleApplication.java
+++ src/main/java/com/salemale/SalemaleApplication.java
@@ -2,10 +2,8 @@ package com.salemale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SalemaleApplication {
 
 	public static void main(String[] args) {
