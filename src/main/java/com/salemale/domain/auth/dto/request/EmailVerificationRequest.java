28f44e0f2d37d4d200b63d264bc39a2ba73a98ed
beans3142 <greenbeans3142@naver.com>
2025-11-05 15:33:07 +0900

    refactor: 역직렬화 추가

--- src/main/java/com/salemale/domain/auth/dto/request/EmailVerificationRequest.java
+++ src/main/java/com/salemale/domain/auth/dto/request/EmailVerificationRequest.java
@@ -2,12 +2,15 @@ package com.salemale.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * EmailVerificationRequest: 이메일 인증번호 발송 요청 DTO
  */
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailVerificationRequest {
 
     @Email(message = "올바른 이메일 형식이 아닙니다.")
