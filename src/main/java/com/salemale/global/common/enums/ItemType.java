90277591de4c2417e83fa97867930b73e836e86f
wnsdud <cccjj3829@gmail.com>
2025-11-11 01:57:15 +0900

    feat: create HotdealStore & add ItemType colunm in item entity & implement repository code

---
+++ src/main/java/com/salemale/global/common/enums/ItemType.java
@@ -0,0 +1,11 @@
+package com.salemale.global.common.enums;
+
+/**
+ * 상품 타입을 구분하는 enum
+ * - AUCTION: 일반 경매 상품
+ * - HOTDEAL: 핫딜 상품 (사업자 인증된 가게의 상품)
+ */
+public enum ItemType {
+    AUCTION,   // 일반 경매
+    HOTDEAL    // 핫딜
+}
